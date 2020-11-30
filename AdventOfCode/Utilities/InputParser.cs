b4e44d875311380f51da8e3463c5d0b7b09d00ff
tslater2006 <tslater2006@gmail.com>
2020-11-30 10:34:10 -0600

    Added helper utilities from 2019 AoC

---
+++ AdventOfCode/Utilities/InputParser.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Inputs
+{
+    class InputParser
+    {
+        public static string[] AsLines(string path)
+        {
+
+            return File.ReadAllLines(path);
+        }
+
+        public static string AsLine(string path)
+        {
+            return AsLines(path)[0];
+        }
+
+        public static int[] AsDigitInts(string path)
+        {
+            return AsLine(path)
+                   .Select(c => (int)Char.GetNumericValue(c))
+                   .ToArray();
+        }
+
+
+        public static List<long> AsLinesLong(string path)
+        {
+            return
+                AsLines(path)
+                .Select(l => Int64.Parse(l))
+                .ToList();
+        }
+
+        public static List<List<int>> AsLinesIntCSV(string path, char split)
+        {
+            var lines = AsLines(path);
+
+            return lines.Select(l => splitLineToIntCells(l, split))
+                        .ToList();
+        }
+
+        public static List<List<long>> AsLinesLongCSV(string path, char split)
+        {
+            var lines = AsLines(path);
+
+            return lines.Select(l => splitLineToLongCells(l, split))
+                        .ToList();
+        }
+
+        public static List<List<string>> AsLinesStringCSV(string path, char split)
+        {
+            var lines = AsLines(path);
+
+            return lines.Select(l => splitLineToTextCells(l, split))
+                        .ToList();
+        }
+
+        public static List<List<string>> AsLinesCharGrid(string path)
+        {
+            var lines = AsLines(path);
+
+            return lines.Select(l => l.ToCharArray().Select(c => new string(c, 1)).ToList()).ToList();
+        }
+
+        public static List<Point> AsLinePoints(string path, char present)
+        {
+            List<Point> points = new List<Point>();
+            string[] dayLines = AsLines(path);
+
+            for (int y = 0; y < dayLines.Length; y++)
+            {
+                for (int x = 0; x < dayLines[y].Length; x++)
+                {
+                    if (dayLines[y][x] == present)
+                    {
+                        points.Add(new Point() { X = x, Y = y });
+                    }
+                }
+            }
+
+            return points;
+        }
+
+        private static List<int> splitLineToIntCells(string line, char split)
+        {
+            var splits = line.Split(split);
+
+            return splits.Select(s => Int32.Parse(s)).ToList();
+        }
+
+        private static List<long> splitLineToLongCells(string line, char split)
+        {
+            var splits = line.Split(split);
+
+            return splits.Select(s => Int64.Parse(s)).ToList();
+        }
+
+        private static List<string> splitLineToTextCells(string line, char split)
+        {
+            var splits = line.Split(split);
+            return splits.ToList();
+        }
+    }
+}
---
+++ AdventOfCode/Utilities/PointExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Utilities
+{
+    static class PointExtensions
+    {
+        public static IEnumerable<Point> Around(this Point p)
+        {
+            yield return new Point(p.X + 1, p.Y);
+            yield return new Point(p.X - 1, p.Y);
+            yield return new Point(p.X, p.Y + 1);
+            yield return new Point(p.X, p.Y - 1);
+        }
+
+        public static Point Add(this Point p, Point p2)
+        {
+            return new Point(p.X + p2.X, p.Y + p2.Y);
+        }
+
+        public static Point Subtract(this Point p, Point p2)
+        {
+            return new Point(p.X - p2.X, p.Y - p2.Y);
+        }
+
+        public static IEnumerable<Point> Around(this Point p, int minX, int minY, int maxX, int maxY)
+        {
+            if (p.X + 1 >= minX && p.X + 1 <= maxX)
+            {
+                yield return new Point(p.X + 1, p.Y);
+            }
+            if (p.X - 1 >= minX && p.X - 1 <= maxX)
+            {
+                yield return new Point(p.X - 1, p.Y);
+            }
+            if (p.Y + 1 >= minY && p.Y + 1 <= maxY)
+            {
+                yield return new Point(p.X, p.Y + 1);
+            }
+            if (p.Y - 1 >= minY && p.Y - 1 <= maxY)
+            {
+                yield return new Point(p.X, p.Y - 1);
+            }
+        }
+    }
+}
