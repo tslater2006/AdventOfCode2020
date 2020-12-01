d3fb38788c61db83ddfc47b487d915eaf9192c41
tslater2006 <tslater2006@gmail.com>
2020-11-30 23:21:08 -0600

    Solution for Day 1

--- AdventOfCode/Days/Day_01.cs
+++ AdventOfCode/Days/Day_01.cs
@@ -2,25 +2,37 @@
 using System;
 using System.IO;
 using Spectre.Console;
+using AdventOfCode.Inputs;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+using AdventOfCode.Utilities;
+using System.ComponentModel;
+
 namespace AdventOfCode
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly long[] _input;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = InputParser.AsLinesLong(InputFilePath).ToArray();
         }
 
         public override string Solve_1()
         {
-            return "foo";
+            IEnumerable<long[]> combos = Combinations.GetCombinations(_input, 2);
+            var matchingPair = combos.Where(l => l.Sum() == 2020).First();
+            return matchingPair.Aggregate((long)1, (a, b) => a * b).ToString();
         }
 
         public override string Solve_2()
         {
-            return "bar";
+            IEnumerable<long[]> combos = Combinations.GetCombinations(_input, 3);
+            var matchingPair = combos.Where(l => l.Sum() == 2020).First();
+            return matchingPair.Aggregate((long)1, (a, b) => a * b).ToString();
         }
     }
+
 }
