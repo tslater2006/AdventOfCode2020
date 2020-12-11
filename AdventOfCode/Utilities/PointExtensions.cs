5f29c48727210b44ccd9452b8e31e83a09854774
tslater2006 <tslater2006@gmail.com>
2020-12-11 11:43:29 -0600

    Utiltity - Extension methods for (int,int) tuple for getting coordinates around. Both orthogonal (rook moves) and adjacent (queen moves). New methods for printing a grid and getting a grid as an 1D enumerable.

--- AdventOfCode/Utilities/PointExtensions.cs
+++ AdventOfCode/Utilities/PointExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,6 +45,101 @@ namespace AdventOfCode.Utilities
             if (p.Y - 1 >= minY && p.Y - 1 <= maxY)
             {
                 yield return new Point(p.X, p.Y - 1);
+            }
+        }
+
+        public static IEnumerable<(int X, int Y)> AroundOrthogonal(this (int X, int Y) p, int minX, int minY, int maxX, int maxY)
+        {
+            if (p.X + 1 >= minX && p.X + 1 <= maxX)
+            {
+                yield return (p.X + 1, p.Y);
+            }
+            if (p.X - 1 >= minX && p.X - 1 <= maxX)
+            {
+                yield return (p.X - 1, p.Y);
+            }
+            if (p.Y + 1 >= minY && p.Y + 1 <= maxY)
+            {
+                yield return (p.X, p.Y + 1);
+            }
+            if (p.Y - 1 >= minY && p.Y - 1 <= maxY)
+            {
+                yield return (p.X, p.Y - 1);
+            }
+        }
+
+        public static IEnumerable<(int X,int Y)> AroundOrthogonal(this (int X,int Y) p)
+        {
+            yield return (p.X + 1, p.Y);
+            yield return (p.X - 1, p.Y);
+            yield return (p.X, p.Y + 1);
+            yield return (p.X, p.Y - 1);
+        }
+
+        public static IEnumerable<(int X, int Y)> AroundAdjacent(this (int X, int Y) p)
+        {
+            yield return (p.X + 1, p.Y);
+            yield return (p.X - 1, p.Y);
+            yield return (p.X, p.Y + 1);
+            yield return (p.X, p.Y - 1);
+
+            yield return (p.X + 1, p.Y + 1);
+            yield return (p.X - 1, p.Y + 1);
+            yield return (p.X - 1, p.Y - 1);
+            yield return (p.X + 1, p.Y - 1);
+
+        }
+
+        public static IEnumerable<(int X, int Y)> AroundAdjacent(this (int X, int Y) p, int minX, int minY, int maxX, int maxY)
+        {
+            var possibles = new (int X, int Y)[]
+            {
+                (p.X + 1, p.Y),
+                (p.X - 1, p.Y),
+                (p.X, p.Y + 1),
+                (p.X, p.Y - 1),
+                (p.X + 1, p.Y + 1),
+                (p.X - 1, p.Y + 1),
+                (p.X - 1, p.Y - 1),
+                (p.X + 1, p.Y - 1)
+            };
+
+            foreach(var newPoint in possibles )
+            {
+                if (newPoint.X >= minX && newPoint.X <= maxX && newPoint.Y >= minY && newPoint.Y <= maxY)
+                {
+                    yield return newPoint;
+                }
+            }
+        }
+
+        public static void PrintGrid(this char[,] grid)
+        {
+            var height = grid.GetLength(0);
+            var width = grid.GetLength(1);
+
+            for(var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    Console.Write(grid[y, x]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static IEnumerable<char> AsEnumerable(this char[,] grid)
+        {
+            var height = grid.GetLength(0);
+            var width = grid.GetLength(1);
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    yield return grid[y, x];
+                }
             }
         }
     }
